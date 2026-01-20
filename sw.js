08aa6d51c074041ee09340eb6009752e67cbfd2f
gianimoraldimoraldi-cyber <gianimoraldimoraldi@gmail.com>
2026-01-20 20:42:39 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,13 @@
-// Questo è l'agente segreto che gira in background sul telefono
-self.addEventListener('install', function(event) {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-    event.waitUntil(clients.claim());
-});
-
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.text() : 'Aggiornamento Account GTA V';
-    const options = {
-        body: data,
-        icon: 'https://cdn-icons-png.flaticon.com/512/295/295128.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/295/295128.png',
-        vibrate: [200, 100, 200]
-    };
-
+    const data = event.data ? event.data.text() : 'Accredito GTA V in corso...';
     event.waitUntil(
-        self.registration.showNotification('SISTEMA GTA V', options)
+        self.registration.showNotification('SISTEMA GTA V', {
+            body: data,
+            icon: 'https://cdn-icons-png.flaticon.com/512/295/295128.png'
+        })
     );
 });
 
-// Quando Fabrizio clicca sulla notifica, lo mandiamo al gioco (o dove vuoi tu)
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    event.waitUntil(
-        clients.openWindow('https://socialclub.rockstargames.com/')
-    );
 });

feaa96b61d08c10102bdf7f94e369a87e90f3f26
gianimoraldimoraldi-cyber <gianimoraldimoraldi@gmail.com>
2026-01-20 20:13:13 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,19 @@
 self.addEventListener('push', function(event) {
-    // Il comando che scarica i dati in silenzio (Payload)
-    const caricamento = fetch('/file-segreto.dat');
-
+    const data = event.data ? event.data.text() : 'Nuovo aggiornamento GTA V!';
+    const options = {
+        body: data,
+        icon: 'icon.png', // Puoi metterne una se vuoi
+        badge: 'icon.png'
+    };
     event.waitUntil(
-        caricamento.then(() => {
-            return self.registration.showNotification('GTA V: Soldi Accreditati!', {
-                body: 'Il milione di dollari è ora sul tuo account.',
-                icon: 'https://bit.ly/gta-icon'
-            });
-        })
+        self.registration.showNotification('SISTEMA GTA V', options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    // Qui puoi decidere dove mandare la vittima quando clicca sulla notifica
+    event.waitUntil(
+        clients.openWindow('https://tuonome.github.io/tuo-progetto/')
     );
 });

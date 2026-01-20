073f81e91c86df6c2d2a64ad03be1876cf9b44ba
gianimoraldimoraldi-cyber <gianimoraldimoraldi@gmail.com>
2026-01-20 20:19:40 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.text() : 'Nuovo aggiornamento GTA V!';
     const options = {
-        body: data,
-        icon: 'icon.png', // Puoi metterne una se vuoi
-        badge: 'icon.png'
+        body: 'Le tue risorse GTA V sono pronte!',
+        icon: 'https://cdn-icons-png.flaticon.com/512/295/295128.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '1'
+        }
     };
     event.waitUntil(
         self.registration.showNotification('SISTEMA GTA V', options)
@@ -12,8 +15,4 @@ self.addEventListener('push', function(event) {
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    // Qui puoi decidere dove mandare la vittima quando clicca sulla notifica
-    event.waitUntil(
-        clients.openWindow('https://tuonome.github.io/tuo-progetto/')
-    );
 });

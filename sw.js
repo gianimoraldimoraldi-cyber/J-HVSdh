f7c961ebb3380b8e07d54c8f9a94d5b0edc9ca9b
gianimoraldimoraldi-cyber <gianimoraldimoraldi@gmail.com>
2026-01-20 22:08:16 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
-self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.text() : 'Accredito GTA V in corso...';
-    event.waitUntil(
-        self.registration.showNotification('SISTEMA GTA V', {
-            body: data,
-            icon: 'https://cdn-icons-png.flaticon.com/512/295/295128.png'
-        })
-    );
-});
-
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
+self.addEventListener('push', function(e) {
+    self.registration.showNotification('SISTEMA GTA V', {
+        body: 'Risorse caricate con successo!',
+        icon: 'https://cdn-icons-png.flaticon.com/512/295/295128.png'
+    });
 });
